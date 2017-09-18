e664dc60698e71f80cf2d4dbd298bb45e5905e69
chillten <bogovich.alexander@gmail.com>
2017-09-18 23:17:31 +0700

    TD form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,24 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') form: NgForm;
+  answer: string;
+  genders = ['male', 'female'];
+
+  user = {
+    name: '',
+    email: '',
+    secret: '',
+    answer: '',
+    gender: ''
+  };
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(form: NgForm) {
@@ -19,5 +34,12 @@ export class AppComponent {
 
   onSubmit() {
     console.log(this.form);
+    this.user.name = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secret = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    this.form.reset();
   }
 }
